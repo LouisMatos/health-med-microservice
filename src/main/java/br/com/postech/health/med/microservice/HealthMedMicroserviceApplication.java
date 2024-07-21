435a514ae7f2ea717b5062bd9101c19c410053d1
LouisMatos <luismatos000@gmail.com>
2024-07-21 16:59:02 -0300

    first commit

---
+++ src/main/java/br/com/postech/health/med/microservice/HealthMedMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.health.med.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMedMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMedMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/health/med/microservice/HealthMedMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.health.med.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMedMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
